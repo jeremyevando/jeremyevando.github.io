d21ffd33cd9ed9bf4540ccce9804a71e29ac61bb
jeremyevando <evandojeremy16@gmail.com>
2023-03-31 09:08:58 +0700

    UTS

--- sw.js
+++ sw.js
@@ -1,16 +1,17 @@
 self.addEventListener("install", function (event) {
-  event.waitUntil(
-    caches.open("first-app").then(function (cache) {
-      cache.addAll(["/", "/index.html", "/src/css/app.css", "/src/js/app.js"]);
-    })
-  );
-  return self.clients.claim();
-});
-
-self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (res) {
-      return res;
-    })
-  );
-});
+    event.waitUntil(
+      caches.open("first-app").then(function (cache) {
+        cache.addAll(["/", "/index.html", "/styles.css", "/app.js", "/blog.html", "/about.html", "/contact.html"]);
+      })
+    );
+    return self.clients.claim();
+  });
+  
+  self.addEventListener("fetch", function (event) {
+    event.respondWith(
+      caches.match(event.request).then(function (res) {
+        return res;
+      })
+    );
+  });
+  
