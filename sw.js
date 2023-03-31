6e60fbc3eb044c78aead5c74dba548c7a3a9d5df
jeremyevando <evandojeremy16@gmail.com>
2023-03-31 11:50:43 +0700

    .

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ self.addEventListener("install", function (event) {
         "/blog.html", "/about.html", "/contact.html","/images/about-header.jpg", 
         "/images/contact-image.jpg", "/images/logo.png", "/manifest.json", "/images/photo.jpg", "/images/photo-wide.jpg", "images/header-bg.jpg",
       "/images/example-blog01.jpg", "/images/example-blog02.jpg", "/images/example-blog03.jpg", "/images/example-blog04.jpg",
-      "/images/example-blog05.jpg", "/images/example-blog06.jpg", "/images/example-work01.jpg", "/images/example-work02.jpg",
+      "/images/example-blog05.jpg", "/images/example-blog06.jpg", "/images/example-blog07.jpg", "/images/example-work01.jpg", "/images/example-work02.jpg",
       "/images/example-work03.jpg", "/images/example-work04.jpg", "/images/example-work05.jpg", "/images/example-work06.jpg", 
       "/images/example-work07.jpg", "/images/example-work08.jpg", "/images/example-work09.jpg", "/images/footer-background.png", 
       "/images/portfolio-example-01.jpg", "/images/portfolio-example-02.jpg", "/images/portfolio-example-03.jpg",
