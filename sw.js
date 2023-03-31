dde7d0d920ef7a94606ccce17d530c745fb600a4
jeremyevando <evandojeremy16@gmail.com>
2023-03-31 11:28:57 +0700

    .

--- sw.js
+++ sw.js
@@ -10,7 +10,8 @@ self.addEventListener("install", function (event) {
       "/images/example-work07.jpg", "/images/example-work08.jpg", "/images/example-work09.jpg", "/images/footer-background.png", 
       "/images/portfolio-example-01.jpg", "/images/portfolio-example-02.jpg", "/images/portfolio-example-03.jpg",
       "/images/portfolio-example-04.jpg", "/images/portfolio-example-05.jpg", "/images/portfolio-example-06.jpg",
-      "/icon-192x192.png", "/icon-256x256.png", "/icon-384x384.png", "/icon-512x512.png", "portfolio-example01.html"]);
+      "/icon-192x192.png", "/icon-256x256.png", "/icon-384x384.png", "/icon-512x512.png", "portfolio-example01.html", "https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en",
+      "https://code.getmdl.io/1.3.0/material.grey-pink.min.css", "https://fonts.googleapis.com/icon?family=Material+Icons"]);
       })
     );
     return self.clients.claim();
